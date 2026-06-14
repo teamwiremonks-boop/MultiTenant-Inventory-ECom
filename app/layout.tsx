e35a8b1ae31904132147ae0528e1cdbbdf02832d
Bhavik <bhavikpathak01@gmail.com>
2026-06-14 16:27:26 +0530

    metadata updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Market Place",
+  description: "Multi-vendor Multi Brand E-commerce Storefront",
 };
 
 const geistSans = Geist({
